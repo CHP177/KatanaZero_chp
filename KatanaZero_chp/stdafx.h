00fb8563ea1d7154ef1144a60d74f65d97c6d1ad
chp <91ckdgus@gmail.com>
2023-04-29 00:15:27 +0900

    install vcpkg SimpleMath header

--- KatanaZero_chp/stdafx.h
+++ KatanaZero_chp/stdafx.h
@@ -30,6 +30,25 @@
 
 using namespace std;
 
+//DirectX D3D11
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+//WRL
+#include <wrl.h>
+using namespace Microsoft;
+using namespace WRL;
+
+//DirectXTK & TK
+#include "_Libraries/DirectXTK/SimpleMath.h"
+using namespace DirectX;
+using namespace SimpleMath;
+
+#include <DirectXTex.h>
+
 //Macros
 #define WIN_DEFAULT_WIDTH 1280.0f
 #define WIN_DEFAULT_HEIGHT 720.0f
@@ -39,3 +58,19 @@ extern HWND gHandle;
 extern HDC gHDC;
 extern float gWinWidth;
 extern float gWinHeight;
+
+//Declare Singleton Macro
+#define DECLARE_SINGLETON(CLASS_NAME)						\
+private:													\
+CLASS_NAME();												\
+~CLASS_NAME();												\
+CLASS_NAME(const CLASS_NAME& other) = delete;				\
+CLASS_NAME& operator=(const CLASS_NAME& other) = delete;	\
+CLASS_NAME(const CLASS_NAME&& other) = delete;				\
+CLASS_NAME& operator=(const CLASS_NAME&& other) = delete;	\
+public:														\
+static CLASS_NAME* Get()									\
+{															\
+	static CLASS_NAME instance;								\
+	return &instance;										\
+}
