d3a24f6fb7710adaf0d8d44444398907bf1fa2e8
chp <91ckdgus@gmail.com>
2023-05-09 14:36:07 +0900

    Camera Github

---
+++ KatanaZero_chp/Systems/Camera.cpp
@@ -0,0 +1,128 @@
+#include "stdafx.h"
+#include "Camera.h"
+
+Camera::Camera()
+{
+	VPBuffer = make_unique<ViewProjectionBuffer>();
+
+	eye = { position.x, position.y, 0 };
+	at = { position.x, position.y, 1 };
+
+	view = XMMatrixLookAtLH(eye, at, up);
+	proj = XMMatrixOrthographicOffCenterLH(LB.x, RT.x, LB.y, RT.y, 0, 1);
+
+	VPBuffer->SetView(view);
+	VPBuffer->SetProjection(proj);
+}
+
+Camera::~Camera() {}
+
+void Camera::Update()
+{
+	
+}
+
+void Camera::Render()
+{
+	VPBuffer->SetVSBuffer(1);
+}
+
+void Camera::UpdateView()
+{
+
+	eye = { position.x, position.y, 0 };
+	at = { position.x, position.y, 1 };
+
+	view = XMMatrixLookAtLH(eye, at, up);
+
+	VPBuffer->SetView(view);
+}
+
+void Camera::UpdateProj()
+{
+	proj = XMMatrixOrthographicOffCenterLH(LB.x, RT.x, LB.y, RT.y, 0, 1);
+
+	VPBuffer->SetProjection(proj);
+}
+
+void Camera::ZoomIn()
+{
+	if (LB.y + zoomSpeed * TIME->GetDeltaTime() * 3 >= RT.y - zoomSpeed * TIME->GetDeltaTime() * 3)   return;
+	LB.x += zoomSpeed * TIME->GetDeltaTime() * widthRatio;
+	LB.y += zoomSpeed * TIME->GetDeltaTime();
+	RT.x -= zoomSpeed * TIME->GetDeltaTime() * widthRatio;
+	RT.y -= zoomSpeed * TIME->GetDeltaTime();
+
+	UpdateProj();
+}
+
+void Camera::ZoomOut()
+{
+	LB.x -= zoomSpeed * TIME->GetDeltaTime() * widthRatio;
+	LB.y -= zoomSpeed * TIME->GetDeltaTime();
+	RT.x += zoomSpeed * TIME->GetDeltaTime() * widthRatio;
+	RT.y += zoomSpeed * TIME->GetDeltaTime();
+
+	UpdateProj();
+}
+
+void Camera::InitZoom()
+{
+	LB = { 0, 0 };
+	RT = { WIN_DEFAULT_WIDTH, WIN_DEFAULT_HEIGHT };
+
+	UpdateProj();
+}
+
+Vector2 Camera::UnProject(const Vector2& source)
+{
+	Vector2 output;
+
+	//viewport 역연산
+	output = (source / Vector2(WIN_DEFAULT_WIDTH, WIN_DEFAULT_HEIGHT) * 2.0f) - Vector2(1, 1);
+
+	//view * proj 역연산
+	Matrix VPInverse = (view * proj).Invert();
+
+	return XMVector2TransformCoord(output, VPInverse);
+}
+
+void Camera::SetPosition(const Vector2& position)
+{
+	this->position = position;
+
+	if (bEdge)
+	{
+		if (this->position.x < minEdge.x)
+			this->position.x = minEdge.x;
+		else if (this->position.x + WIN_DEFAULT_WIDTH > maxEdge.x)
+			this->position.x = maxEdge.x - WIN_DEFAULT_WIDTH;
+
+		if (this->position.y < minEdge.y)
+			this->position.y = minEdge.y;
+		else if (this->position.y + WIN_DEFAULT_HEIGHT > maxEdge.y)
+			this->position.y = maxEdge.y - WIN_DEFAULT_HEIGHT;
+	}
+
+	UpdateView();
+}
+
+void Camera::Move(const Vector2& position)
+{
+	this->position += position * TIME->GetDeltaTime();
+
+	if (bEdge)
+	{
+		if (this->position.x < minEdge.x)
+			this->position.x = minEdge.x;
+		else if (this->position.x + WIN_DEFAULT_WIDTH > maxEdge.x)
+			this->position.x = maxEdge.x - WIN_DEFAULT_WIDTH;
+
+		if (this->position.y > minEdge.y)
+			this->position.y = minEdge.y;
+		else if (this->position.y + WIN_DEFAULT_HEIGHT > maxEdge.y)
+			this->position.y = maxEdge.y - WIN_DEFAULT_HEIGHT;
+	}
+
+	UpdateView();
+}
---
+++ KatanaZero_chp/Systems/Camera.h
@@ -0,0 +1,50 @@
+#pragma once
+
+class Camera
+{
+	DECLARE_SINGLETON(Camera);
+
+public:
+	void Update();
+	void Render();
+
+public:
+	void UpdateView();
+	void UpdateProj();
+
+public:
+	void ZoomIn();
+	void ZoomOut();
+	void InitZoom();
+
+	Vector2 UnProject(const Vector2& source);
+
+public:
+	Vector2 GetPosition() const { return position; }
+
+	void SetPosition(const Vector2& position);
+	void SetEdges(bool bEdge) { this->bEdge = bEdge; }
+	void SetEdges(const Vector2& min, const Vector2& max)
+	{
+		minEdge = min;
+		maxEdge = max;
+		bEdge = true;
+	}
+
+	void Move(const Vector2& position);
+
+private:
+	unique_ptr<class ViewProjectionBuffer> VPBuffer;
+	Matrix view, proj;
+
+	Vector3 eye, at, up = { 0, 1, 0 };
+	Vector2 position;
+	Vector2 LB = { 0, 0 }, RT = { gWinWidth, gWinHeight };
+	float widthRatio = gWinWidth / gWinHeight;
+
+	Vector2 minEdge, maxEdge;
+	bool bEdge = false;
+
+	float speed = 300.0f;
+	float zoomSpeed = 300.0f;
+};
